0423882675ad9ee0922bf196a6d62cb6a690808a
亀澤亜美 <kamesawaami@kamezawaaminoMacBook-Air.local>
2024-04-28 10:45:41 +0900

    cssの調整

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,12 @@ joys = [
   {name: 'ゆっくりコーヒーを淹れる', joy_image: 'coffee'},
   {name: '思っていることを文字に書き起こす', joy_image: 'journaling'},
   {name: '空を眺めて深呼吸する', joy_image: 'breath'},
+  {name: '曲を流しながら歌を歌う', joy_image: 'sing'},
+  {name: '机の上を片付ける', joy_image: 'desk'},
+  {name: '遊びに行ってみたいところを調べる', joy_image: 'think'},
+  {name: '肩を回してこりをほぐす', joy_image: 'katamawashi'},
+  {name: '本屋や図書館に行く', joy_image: 'books'},
+  {name: '15分昼寝をする', joy_image: 'images'},
 ]
 
 
