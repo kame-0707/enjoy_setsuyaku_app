bb97b3a28ece51c813911e3ce9ee82f984675de7
亀澤亜美 <kamesawaami@kamezawaaminoMacBook-Air.local>
2024-04-28 11:04:47 +0900

    db追加ファイルの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ joys = [
   {name: '遊びに行ってみたいところを調べる', joy_image: 'think'},
   {name: '肩を回してこりをほぐす', joy_image: 'katamawashi'},
   {name: '本屋や図書館に行く', joy_image: 'books'},
-  {name: '15分昼寝をする', joy_image: 'images'},
+  {name: '15分昼寝をする', joy_image: 'images'}
 ]
 
 
